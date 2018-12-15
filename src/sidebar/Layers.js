dab4e55aa253ffa0628e1c77329cae7c5c8eb7e7
Andreas Strand <andreasstrand94@gmail.com>
2018-12-15 21:58:44 +0100

    Each layer now has a checkbox, that will hide or show the layer, based on its value. closes #17

--- src/sidebar/Layers.js
+++ src/sidebar/Layers.js
@@ -21,21 +21,44 @@ class Layers extends Component{
   }
 
   componentDidUpdate(prevProps, prevState) {
+
     // Appending new file name to Layer list with a unique key value.
     if (this.state.layer_list !== prevState.layer_list ){
       getLayerList(this.state.layer_list)
       getLayerListUnion(this.state.layer_list)
       getLayerListIntersect(this.state.layer_list)
       getLayerListDifference(this.state.layer_list)
+
+      // Sets a click listener for the newest layer's checkbox event.
+      var id = this.state.layer_list[this.state.layer_list.length-1][1];
+      var element = document.getElementById('checkbox_'+id);
+
+
+      element.addEventListener('click', function(event) {
+        // adding stopPropagation so layer is not selected when checkbox is clicked.
+        event.stopPropagation();
+        // To check whether the checkbox is clicked or not.
+        var element = document.getElementById('checkbox_'+id);
+        // To get the correct layer on the map to hide.
+        var map_element = document.getElementsByClassName("map-path "+id);
+        if (element.checked == true) {
+          $(map_element).show();
+        } else {
+          $(map_element).hide();
+        }
+      });
     }
 
     // Make all the new layers selectable and dragable.
     addLayerProperties()
+
+
   }
 
   componentDidMount() {
     // Make the initial layers selectable and dragable.
     addLayerProperties()
+
   }
 
   render(){
@@ -45,12 +68,17 @@ class Layers extends Component{
     for (var i = 0; i < layer_data_list.length; i++) {
       data.push({"name":layer_data_list[i][0],"key":layer_data_list[i][1]})
     }
-    const listItems = data.map((d) => <li className="layer" id={d.key}>{d.name}</li>)
+    const listItems = data.map((d) =>
+      <li className="layer" id={d.key}>
+        <input type="checkbox" defaultChecked={true} id={"checkbox_"+d.key} />
+        {d.name.slice(0,30)}
+      </li>
+  )
 
     return(
       <div id="layers">
           <p id="subtitle">Layers</p>
-          <button id="delete" onClick={(param) => this.deleteLayer(param)}>[TRASH]</button>
+          <button id="delete" onClick={(param) => this.deleteLayer(param)}>🗑️</button>
           <p id='sub_info'>Click to select, drag to reorder.</p>
           <div>
               <ul id="sortable_layers" className="ui-sortable">
@@ -73,19 +101,19 @@ class Layers extends Component{
 
   // Checks if upload GeoJSON button has been clicked, and file has been selected.
   activateFileUpload(){
-    console.log("Activate file upload call")
     document.getElementById('fileid').click();
   }
 
-// Sends call to Sidebar to delete selected layer, when delete button is clicked.
+  // Sends call to Sidebar to delete selected layer, when delete button is clicked.
   deleteLayer(){
     var delete_layer_key= ($('li.active').attr('id'));
     deleteLayerCall(delete_layer_key)
   }
 
- /*
-        TODO: ADD EXCEPTION HANDELING FOR FILE UPLOAD?
- */
+  // Code running when checkbox is clicked.
+  handleChangeChk(){
+    console.log("HandleClick")
+  }
 
   // Adding file reading code in Layers as this is the only place it is used.
   readGeoJSONFile(file_upload){
